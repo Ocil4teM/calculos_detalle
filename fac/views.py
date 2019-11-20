88c312444699d050cba52b37670292fe51ffabe1
THRUMAN CARRERA <thruman_c@hotmail.com>
2019-11-19 21:05:25 -0500

    'selec_producto'

---
+++ fac/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render
+from django.views import generic
+
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponse
+from datetime import datetime
+
+from bases.views import SinPrivilegios
+
+from .models import Cliente, FacturaEnc, FacturaDet
+from .forms import ClienteForm
+import inv.views as inv
+
+class ClienteView(SinPrivilegios, generic.ListView):
+    model = Cliente
+    template_name = "fac/cliente_list.html"
+    context_object_name = "obj"
+    permission_required="cmp.view_cliente"
+
+
+class VistaBaseCreate(SuccessMessageMixin,SinPrivilegios, \
+    generic.CreateView):
+    context_object_name = 'obj'
+    success_message="Registro Agregado Satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+class VistaBaseEdit(SuccessMessageMixin,SinPrivilegios, \
+    generic.UpdateView):
+    context_object_name = 'obj'
+    success_message="Registro Actualizado Satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+
+class ClienteNew(VistaBaseCreate):
+    model=Cliente
+    template_name="fac/cliente_form.html"
+    form_class=ClienteForm
+    success_url= reverse_lazy("fac:cliente_list")
+    permission_required="fac.add_cliente"
+
+
+class ClienteEdit(VistaBaseEdit):
+    model=Cliente
+    template_name="fac/cliente_form.html"
+    form_class=ClienteForm
+    success_url= reverse_lazy("fac:cliente_list")
+    permission_required="fac.change_cliente"
+
+
+@login_required(login_url="/login/")
+@permission_required("fac.change_cliente",login_url="/login/")
+def clienteInactivar(request,id):
+    cliente = Cliente.objects.filter(pk=id).first()
+
+    if request.method=="POST":
+        if cliente:
+            cliente.estado = not cliente.estado
+            cliente.save()
+            return HttpResponse("OK")
+        return HttpResponse("FAIL")
+    
+    return HttpResponse("FAIL")
+
+
+class FacturaView(SinPrivilegios, generic.ListView):
+    model = FacturaEnc
+    template_name = "fac/factura_list.html"
+    context_object_name = "obj"
+    permission_required="fac.view_facturaenc"
+
+
+@login_required(login_url='/login/')
+@permission_required('fac.change_facturasenc', login_url='bases:sin_privilegios')
+def facturas(request,id=None):
+    template_name='fac/facturas.html'
+
+    encabezado = {
+        'fecha':datetime.today()
+    }
+    detalle = {}
+    clientes = Cliente.objects.filter(estado=True)
+    print(clientes)
+    contexto = {"enc":encabezado,"det":detalle,"clientes":clientes}
+
+    return render(request,template_name,contexto)
+
+
+class ProductoFacView(inv.ProductoView):
+    template_name="fac/buscar_producto.html" 
